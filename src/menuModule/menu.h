d227e262221466bf89c09895772ef196fc01c53d
Feniks <gtgtr3000@gmail.com>
2021-03-29 23:35:01 +1000

    update menu module & add consts.h

--- src/menuModule/menu.h
+++ src/menuModule/menu.h
@@ -1,15 +1,17 @@
 #include <memory>
 #include "guiModule/gui.h"
+#include "consts.h"
 namespace game {
 	namespace menuModule {
 		class menu {
 		public:
-			menu(std::shared_ptr<GUI::window>);
+			menu();
+			states run(std::shared_ptr<GUI::window>);
 		private:
-			GUI::button start;
+			GUI::button play;
 			GUI::button exit;
 			GUI::label title;
-
+			GUI::mBackground bg;
 		};
 	}
 }
